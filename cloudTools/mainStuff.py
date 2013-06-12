7fc16a3e9e671f35461b327362acfdf047ae6108
Brian <Brian.leach.6@gmail.com>
2013-06-11 20:50:28 -0600

    prints each element of an array

--- cloudTools/mainStuff.py
+++ cloudTools/mainStuff.py
@@ -2,5 +2,16 @@
 This is hopefully something that will work. Hours have been spent
 '''
 
-for i in range(20):
-    print i, 
+import numpy as np
+
+a = np.array([2,3,4,5])
+
+print a
+
+b = np.append([a], [[3,6,7,7]], axis=0)
+
+print b 
+
+for item in b:
+    for i in item:
+        print i, 
